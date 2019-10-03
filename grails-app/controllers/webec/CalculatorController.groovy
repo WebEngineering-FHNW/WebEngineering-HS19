c7726533e03a02dfd733bc0c98f7c00e6371a159
Dierk Koenig <dierk.koenig@canoo.com>
2019-10-03 23:23:12 +0200

    prepare for week 3

---
+++ grails-app/controllers/webec/CalculatorController.groovy
@@ -0,0 +1,9 @@
+package webec
+
+class CalculatorController {
+
+    def calc(double en, double exam) {
+        double result = (en + exam) / 2
+        render view: "CalculatorOutput", model: [result: result]
+    }
+}
