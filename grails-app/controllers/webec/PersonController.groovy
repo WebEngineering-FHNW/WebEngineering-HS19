4f7c54413d4ccf57c572a132b14aedf582b902b9
Dierk Koenig <dierk.koenig@canoo.com>
2019-10-25 13:08:31 +0200

    person and room domain classes plus controllers, initial

---
+++ grails-app/controllers/webec/PersonController.groovy
@@ -0,0 +1,6 @@
+package webec
+
+class PersonController {
+
+    static scaffold = Person
+}
---
+++ grails-app/domain/webec/Person.groovy
@@ -0,0 +1,9 @@
+package webec
+
+class Person {
+    String firstName
+    String lastName
+
+    static constraints = {
+    }
+}
