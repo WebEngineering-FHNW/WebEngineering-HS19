f5823016f810e24b60448aef8ad18c5ab4fe9eb9
Dierk Koenig <dierk.koenig@canoo.com>
2019-12-13 14:20:41 +0100

    week 12 : JSTempConverterSpec avoid js activation issue

--- src/integration-test/groovy/webec/InPlaceCalculatorSpec.groovy
+++ src/integration-test/groovy/webec/InPlaceCalculatorSpec.groovy
@@ -44,21 +44,21 @@ class InPlaceCalculatorSpec extends GebSpec {
 
 // todo: 3 un-comment the commented lines below and see them failing, then make them pass
 
-//    void "Invalid input is handled in-place by JS without submission"() {
-//        given: "a valid state"
-//            try { browser.driver.javascriptEnabled = true } catch(onlyForHtmlUnit) {}
-//            go '/InPlaceCalculator.html?lang=en'
-//            $("form").en   = '3.0'
-//            $("form").exam = '3.0'
-//        when:
-//            $("input", type: "submit").click()
-//        then: "we should have a clean, valid state to start from"
-//            $("#en").attr('class') == ""
-//        when: "we enter some invalid value _without_ submitting"
-//            $("form").en = '0.9'   // note: no click here!
-//        then: "the in-place JS logic should kick in"
-//            $("#en").attr('class') == "error"
-//    }
+    void "Invalid input is handled in-place by JS without submission"() {
+        given: "a valid state"
+            try { browser.driver.javascriptEnabled = true } catch(onlyForHtmlUnit) {}
+            go '/InPlaceCalculator.html?lang=en'
+            $("form").en   = '3.0'
+            $("form").exam = '3.0'
+        when:
+            $("input", type: "submit").click()
+        then: "we should have a clean, valid state to start from"
+            $("#en").attr('class') == ""
+        when: "we enter some invalid value _without_ submitting"
+            $("form").en = '0.9'   // note: no click here!
+        then: "the in-place JS logic should kick in"
+            $("#en").attr('class') == "error"
+    }
 
 
 }
--- src/integration-test/groovy/webec/JSTempConverterSpec.groovy
+++ src/integration-test/groovy/webec/JSTempConverterSpec.groovy
@@ -11,7 +11,7 @@ class JSTempConverterSpec extends GebSpec {
 
     void "From celsius to fahrenheit with JavaScript"() {
         when:
-            browser.driver.javascriptEnabled = true
+        try { browser.driver.javascriptEnabled = true } catch(onlyForHtmlUnit) {}
             go '/static/Temperatures.html'
         then:
         	title == "Temperature Converter with JavaScript"
